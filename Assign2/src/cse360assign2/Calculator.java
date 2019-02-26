ca35d714c1877d7585d9c6d698d17a3660f27002
iakeeh <iakeeh@asu.edu>
2019-02-25 23:22:48 -0700

    Second Change. Final Version.

--- Assign2/src/cse360assign2/Calculator.java
+++ Assign2/src/cse360assign2/Calculator.java
@@ -11,12 +11,15 @@ package cse360assign2;
 
 public class Calculator {
 	private int total;
+	private String history;
 
 	/**
 	 * This constructor set the initial value of total to zero.
+	 * Also, initialize history to "0 ".
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0 ";
 	}
 	
 	/**
@@ -31,43 +34,52 @@ public class Calculator {
 	
 	/**
 	 * This method add the value given in the parameter to total.
+	 * Also, add what method did to history.
 	 * 
 	 * @param value   the integer value given
 	 */
 	public void add (int value) {
 		total = total + value;
+		history = history + "+ " + value + " ";
 	}
 	
 	/**
 	 * This method subtract the value given in the parameter to total.
+	 * Also, add what method did to history.
 	 * 
 	 * @param value  the integer value given
 	 */
 	public void subtract (int value) {
-		total = total - value; 
+		total = total - value;
+		history = history + "- " + value + " ";
 	}
 	
 	/**
 	 * This method multiply the value given in the parameter to total.
+	 * Also, add what method did to history.
 	 * 
 	 * @param value the integer value given
 	 */
 	public void multiply (int value) {
 		total = total * value;
+		history = history + "* " + value + " ";
 	}
 	
 	/**
 	 * This method divide total by the value given in the parameter.
 	 * If the value given is zero then total get set to zero.
+	 * Also, add what method did to history.
 	 * 
 	 * @param value  the integer value given
 	 */
 	public void divide (int value) {
 		if(value==0) {
 			total = 0;
+			history = "0 ";
 		}
 		else {
 			total = total / value;
+			history = history + "/ " + value + " ";
 		}
 	}
 	
@@ -77,6 +89,6 @@ public class Calculator {
 	 * @return  string of all action perform on total
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
--- Assign2/src/cse360assign2/Main.java
+++ Assign2/src/cse360assign2/Main.java
@@ -15,11 +15,48 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
+		System.out.println("Testing Calculator!");
+		System.out.println("Part 1:");
+		
+		System.out.println("Adding 4");
 		cal.add(4);
+		
+		System.out.println("Subtracting 2");
 		cal.subtract(2);
+		
+		System.out.println("Multiplying 2");
 		cal.multiply(2);
+		
+		System.out.println("Adding 5");
 		cal.add(5);
-		cal.getHistory();
+		
+		System.out.printf("Total: %d\n", cal.getTotal());
+		System.out.println("Showing history: ");
+		System.out.println(cal.getHistory());
+		
+		System.out.println("\nPart 2:");
+		System.out.println("Diving 0");
+		cal.divide(0);
+		System.out.printf("Total: %d\n", cal.getTotal());
+		System.out.println("Showing history: ");
+		System.out.println(cal.getHistory());
+		
+		System.out.println("\nPart 3:");
+		System.out.println("Adding 3");
+		cal.add(3);
+		
+		System.out.println("Subtracting 1");
+		cal.subtract(1);
+		
+		System.out.println("Multiplying 6");
+		cal.multiply(6);
+		
+		System.out.println("Adding 15");
+		cal.add(15);
+		
+		System.out.printf("Total: %d\n", cal.getTotal());
+		System.out.println("Showing history: ");
+		System.out.println(cal.getHistory());
 
 	}
 
